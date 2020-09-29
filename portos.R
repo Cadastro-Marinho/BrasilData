0c52d6586c36daec9f8d28fbd162e68bbbf44b02
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-09-29 16:31:10 -0300

    split Itajai port areas.

--- portos.R
+++ portos.R
@@ -6,3 +6,5 @@ sfs <- read_sf("portos/sfs.geojson")
 itajai <- read_sf("portos/itajai.geojson")
 
 portos <- rbind(sfs, itajai, imbituba, laguna)
+
+st_write(portos, "portos.geojson", delete_dsn = TRUE)
